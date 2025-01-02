d9e0bcdf3915a3783b0526ff289c795947f03c2f
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-02 18:37:30 -0500

    feat: log seconds since I was born

--- SecondsSinceBorn/main.m
+++ SecondsSinceBorn/main.m
@@ -9,8 +9,21 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1996];
+        [comps setMonth:2];
+        [comps setDay:20];
+        [comps setHour:20];
+        [comps setMinute:0];
+        [comps setSecond:0];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        
+        NSDate *now = [NSDate date];
+        double secondsSinceBorn = [now timeIntervalSinceDate:dateOfBirth];
+
+        NSLog(@"Time flies! %.2f seconds have ticked by since I first entered the world!", secondsSinceBorn);
     }
     return 0;
 }
